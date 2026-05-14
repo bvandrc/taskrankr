48cef12c97d3e827aca784a2bd0de20d4f06f11b
bvandercar-vt <bvandercar@outlook.com>
2026-05-13 19:35:58 -0600

    imports

--- client/src/providers/DraftSessionProvider.tsx
+++ client/src/providers/DraftSessionProvider.tsx
@@ -43,8 +43,12 @@ import { omit } from 'es-toolkit'
 import type { EmptyObject } from 'type-fest'
 
 import { debugLog } from '@/lib/debug-logger'
-import { buildLocalTask, statusToStatusPatch } from '@/lib/task-provider-utils'
-import { collectDescendantIds, removeIds } from '@/lib/task-tree-utils'
+import { buildLocalTask } from '@/lib/task-provider-utils'
+import {
+  collectDescendantIds,
+  removeIds,
+  statusToStatusPatch,
+} from '@/lib/task-tree-utils'
 import {
   type CreateTaskContent,
   type UpdateTaskContent,
@@ -431,10 +435,7 @@ export const DraftSessionProvider = ({
   const setTaskStatus = useCallback(
     (id: number, status: TaskStatus): LocalTask => {
       if (isDraftIdStable(id)) {
-        return updateDraftTask(
-          id,
-          statusToStatusPatch(status) as UpdateTaskContent,
-        )
+        return updateDraftTask(id, statusToStatusPatch(status))
       }
       return realSetTaskStatus(id, status)
     },

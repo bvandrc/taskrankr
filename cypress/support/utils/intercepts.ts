942fd277fadf7251dd380f6effe19090cd6efba7
Blake V. <87083504+bvandrc@users.noreply.github.com>
2026-05-19 00:27:58 -0600

    fix: sync queue (#120)
    Co-authored-by: bvandercar1 <53074288-bvandercar1@users.noreply.replit.com>

--- cypress/support/utils/intercepts.ts
+++ cypress/support/utils/intercepts.ts
@@ -27,15 +27,20 @@ export function maybeWaitForIntercept(
   cy.get(Selectors.Toasts.ERROR).should('not.exist')
 }
 
+export type CreatedTask = Pick<Task, 'name' | 'status' | RankField>
+
+let createTaskWaitCount = 0
+
 export const interceptCreate = () =>
   cy.intercept('POST', ApiPaths.CREATE_TASK).as('createTask')
-
-export type CreatedTask = Pick<Task, 'name' | 'status' | RankField>
 
 export function waitForCreate(tasks: CreatedTask[]): void {
   maybeWaitForIntercept('@createTask', tasks.length, 201)
   checkTasksExistBackend(tasks)
+  createTaskWaitCount += tasks.length
 }
+
+let deleteTaskWaitCount = 0
 
 export const interceptDelete = () =>
   cy.intercept('DELETE', ApiPaths.DELETE_TASK).as('deleteTask')
@@ -43,7 +48,10 @@ export const interceptDelete = () =>
 export const waitForDelete = (tasks: Pick<Task, 'name'>[]) => {
   maybeWaitForIntercept('@deleteTask', tasks.length, 204)
   checkTasksDontExistBackend(tasks)
+  deleteTaskWaitCount += tasks.length
 }
+
+let updateTaskWaitCount = 0
 
 export const interceptUpdate = () =>
   cy.intercept('PUT', ApiPaths.UPDATE_TASK).as('updateTask')
@@ -51,20 +59,37 @@ export const interceptUpdate = () =>
 export const waitForUpdate = (tasks: CreatedTask[]) => {
   maybeWaitForIntercept('@updateTask', tasks.length, 200)
   checkTasksExistBackend(tasks)
+  updateTaskWaitCount += tasks.length
 }
 
 export const checkNumCalls = ({
-  create,
-  update,
+  create: createCount,
+  update: updateCount,
+  delete: _deleteCount,
 }: {
   create?: number
   update?: number
+  delete?: number
 }) => {
+  // TODO: debug
+  // for (const [expected, waitedFor] of [
+  //   [createCount, createTaskWaitCount],
+  //   [updateCount, updateTaskWaitCount],
+  //   [deleteCount, deleteTaskWaitCount],
+  // ]) {
+  //   if (expected !== undefined && expected !== waitedFor) {
+  //     expect(
+  //       expected,
+  //       '# expected calls should match # test has waited for',
+  //     ).to.equal(waitedFor)
+  //   }
+  // }
+
   const loggedIn = isLoggedIn()
-  if (create !== undefined) {
-    cy.get('@createTask.all').should('have.length', loggedIn ? create : 0)
+  if (createCount !== undefined) {
+    cy.get('@createTask.all').should('have.length', loggedIn ? createCount : 0)
   }
-  if (update !== undefined) {
-    cy.get('@updateTask.all').should('have.length', loggedIn ? update : 0)
+  if (updateCount !== undefined) {
+    cy.get('@updateTask.all').should('have.length', loggedIn ? updateCount : 0)
   }
 }

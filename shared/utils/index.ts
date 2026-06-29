bde2ebaec657e6dd80a55eae4efc644b7a92fa1c
bvandercar-vt <bvandercar@outlook.com>
2026-06-29 15:36:49 -0600

    style: arrow fn

--- shared/utils/index.ts
+++ shared/utils/index.ts
@@ -1,9 +1,8 @@
-export function createObject<const Keys extends readonly string[], Schema>(
+export const createObject = <const Keys extends readonly string[], Schema>(
   keys: Keys,
   value: Schema,
-) {
-  return Object.fromEntries(keys.map((key) => [key, value])) as Record<
+) =>
+  Object.fromEntries(keys.map((key) => [key, value])) as Record<
     Keys[number],
     Schema
   >
-}

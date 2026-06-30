189e5432a97cb2030c40ab4a4bb99ec3a83e6d65
bvandercar-vt <bvandercar@outlook.com>
2026-06-29 19:30:10 -0600

    fix class

--- client/src/components/primitives/forms/DayPicker.tsx
+++ client/src/components/primitives/forms/DayPicker.tsx
@@ -40,7 +40,7 @@ export const DayPicker = ({
         'relative flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0',
       month: 'space-y-4',
       month_caption: 'flex justify-center pt-1 items-center',
-      caption_label: 'text-m font-medium',
+      caption_label: 'text-md font-medium',
       nav: 'absolute top-0 inset-x-0 flex justify-between items-center pt-1 px-1',
       button_previous: cn(
         buttonVariants({ variant: 'outline' }),
@@ -52,7 +52,7 @@ export const DayPicker = ({
       ),
       month_grid: 'w-full border-collapse space-y-1',
       weekdays: 'flex',
-      weekday: 'text-muted-foreground rounded-md w-9 font-normal text-s',
+      weekday: 'text-muted-foreground rounded-md w-9 font-normal text-sm',
       week: 'flex w-full mt-2',
       day: 'size-9 text-center text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20',
       day_button: cn(

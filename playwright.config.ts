ca4dfcb0e899c6fcdd86f9f1991aa63198a31578
bvandercar-vt <bvandercar@outlook.com>
2026-06-30 16:07:23 -0600

    no retries ever

--- playwright.config.ts
+++ playwright.config.ts
@@ -20,7 +20,7 @@ export default defineConfig({
   testDir: './playwright',
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 2 : 0,
+  retries: 0, // process.env.CI ? 2 : 0,
   reporter: [['html', { outputFolder: 'playwright/results/html' }]],
   use: {
     baseURL: BASE_URL,

902210a98bbbc63b234d31d7768822fc1aba6b1d
bvandercar-vt <bvandercar@outlook.com>
2026-06-30 15:55:24 -0600

    refactor

--- playwright/support/utils/intercepts.ts
+++ playwright/support/utils/intercepts.ts
@@ -1,38 +1,41 @@
 import { expect } from '@playwright/test'
 import { mapValues } from 'es-toolkit'
-import type { Entries } from 'type-fest'
+import type { Entries, SetOptional } from 'type-fest'
 
-import type { Task } from '~/shared/schema'
+import type { RankField, Task } from '~/shared/schema'
 import { Selectors } from '../constants'
 import { waitForCreate, waitForUpdate } from '../fixtures'
 import { getIsLoggedIn, getPage, getRequestTracker } from '../test-globals'
 import { checkTasksDontExistBackend, checkTasksExistBackend } from './api'
 
-export async function maybeWaitForResponses(
-  type: 'create' | 'update',
+async function maybeWaitForResponses(
+  waiter: (count: number) => Promise<void>,
   count: number,
   _expectedStatus: number,
 ): Promise<void> {
-  if (!getIsLoggedIn() || count === 0) return
-  const waiter = type === 'create' ? waitForCreate(count) : waitForUpdate(count)
-  await waiter
+  if (getIsLoggedIn() && count > 0) {
+    await waiter(count)
+    // TODO: need to bring back in status check
+  }
   await expect(getPage().locator(Selectors.Toasts.ERROR)).not.toBeVisible()
 }
 
-export type CreatedTask = Pick<Task, 'name' | 'status'> &
-  Partial<Pick<Task, 'priority' | 'ease' | 'enjoyment' | 'time' | 'schedule'>>
+export type CreatedTask = SetOptional<
+  Pick<Task, 'name' | 'status' | 'schedule' | RankField>,
+  'schedule'
+>
 
 export async function waitForCreateAndVerify(
   tasks: CreatedTask[],
 ): Promise<void> {
-  await maybeWaitForResponses('create', tasks.length, 201)
+  await maybeWaitForResponses(waitForCreate, tasks.length, 201)
   await checkTasksExistBackend(tasks)
 }
 
 export async function waitForUpdateAndVerify(
   tasks: CreatedTask[],
 ): Promise<void> {
-  await maybeWaitForResponses('update', tasks.length, 200)
+  await maybeWaitForResponses(waitForUpdate, tasks.length, 200)
   await checkTasksExistBackend(tasks)
 }
 
